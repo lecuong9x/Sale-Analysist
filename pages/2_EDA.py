d9a5b5b93b4144ed48c1f580ede53f0e889c58f7
lecuong9x <58180391+lecuong9x@users.noreply.github.com>
2023-01-29 21:45:37 +0700

    Update 2_EDA.py

--- pages/2_EDA.py
+++ pages/2_EDA.py
@@ -101,39 +101,21 @@ fig_city_salses = px.bar(
 )
 st.plotly_chart(fig_city_salses)
 
-#aliceblue, antiquewhite, aqua, aquamarine, azure, beige, bisque, black, 
-# blanchedalmond, blue, blueviolet, brown, burlywood, cadetblue, chartreuse, 
-# chocolate, coral, cornflowerblue, cornsilk, crimson, cyan, darkblue, darkcyan, 
-# darkgoldenrod, darkgray, darkgrey, darkgreen, darkkhaki, darkmagenta, 
-# darkolivegreen, darkorange, darkorchid, darkred, darksalmon, darkseagreen, 
-# darkslateblue, darkslategray, darkslategrey, darkturquoise, darkviolet, deeppink, 
-# deepskyblue, dimgray, dimgrey, dodgerblue, firebrick, floralwhite, forestgreen, 
-# fuchsia, gainsboro, ghostwhite, gold, goldenrod, gray, grey, green, greenyellow, 
-# honeydew, hotpink, indianred, indigo, ivory, khaki, lavender, lavenderblush, 
-# lawngreen, lemonchiffon, lightblue, lightcoral, lightcyan, lightgoldenrodyellow, 
-# lightgray, lightgrey, lightgreen, lightpink, lightsalmon, lightseagreen, 
-# lightskyblue, lightslategray, lightslategrey, lightsteelblue, lightyellow, lime, 
-# limegreen, linen, magenta, maroon, mediumaquamarine, mediumblue, mediumorchid, 
-# mediumpurple, mediumseagreen, mediumslateblue, mediumspringgreen, mediumturquoise,
-# mediumvioletred, midnightblue, mintcream, mistyrose, moccasin, navajowhite, navy, 
-# oldlace, olive, olivedrab, orange, orangered, orchid, palegoldenrod, palegreen, 
-# paleturquoise, palevioletred, papayawhip, peachpuff, peru, pink, plum, powderblue, 
-# purple, red, rosybrown, royalblue, rebeccapurple, saddlebrown, salmon, sandybrown, 
-# seagreen, seashell, sienna, silver, skyblue, slateblue, slategray, slategrey, snow,
-# springgreen, steelblue, tan, teal, thistle, tomato, turquoise, violet, wheat, white, 
-# whitesmoke, yellow, yellowgreen 
-
-
-
-st.success("Thời gian bán hàng được nhiều")
-
-
 sale_by_hour = (
     df_selection.groupby("Hours")
     .agg({"Sales":"sum"})
 )
 
-st.line_chart(sale_by_hour)
+st.line_chart(sale_by_hour,use_container_width=True)
 
 sale_product = df_selection.groupby('Product').sum()['Quantity Ordered']
 prices =df_selection.groupby('Product').mean()['Price Each']
+
+st.success("Thời điểm trong năm bán hàng được nhiều")
+sale_by_month = (
+    df_all2.groupby("Month").agg({"Sales":"sum"})
+)
+#st.write(sale_by_month)
+st.line_chart(sale_by_month,use_container_width=True)
+
+st.success("Thời điểm trong ngày bán hàng được nhiều")
